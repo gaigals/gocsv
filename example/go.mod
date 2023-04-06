d931e5dbb5d5f008a540b069c87a4d5e33bcb1ee
Kārlis Gaigals <karlis@nic.lv>
2023-04-06 13:38:13 +0300

    Example

---
+++ example/go.mod
@@ -0,0 +1,12 @@
+module main
+
+go 1.20
+
+replace github.com/gaigals/gocsv v0.0.0-20221205080502-490bd6fa719d => ../
+
+require github.com/gaigals/gocsv v0.0.0-20221205080502-490bd6fa719d
+
+require (
+	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de // indirect
+	github.com/gaigals/gotags v0.1.2-0.20230404082231-0d5c35a4f15e // indirect
+)
---
+++ example/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/gaigals/gocsv"
+)
+
+type Person struct {
+	ID        uint      `csv:"ID"`
+	FirstName string    `csv:"FirstName"`
+	LastName  string    `csv:"LastName"`
+	Age       uint      `csv:"Age"`
+	Country   string    `csv:"Country"`
+	Gender    string    `csv:"Gender"`
+	Phone     string    `csv:"Phone"`
+	Street    string    `csv:"Street"`
+	Email     string    `csv:"Email"`
+	Language  string    `csv:"Language"`
+	BirthDate time.Time `csv:"BirthDate"`
+	Added     time.Time `csv:"Added"`
+	Modified  time.Time `csv:"Modified"`
+}
+
+func main() {
+	persons := make([]Person, 0)
+
+	err := gocsv.ReadFile(&persons, "../test_files/person.csv", ';')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, person := range persons {
+		fmt.Println(person)
+	}
+}
